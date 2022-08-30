0668584de65593482eda9d0a1012b174c1e95678
Jim <jamesmpyle@gmail.com>
2022-08-30 16:36:46 -0400

    update

---
+++ src/wk1VarsAndOps.java
@@ -0,0 +1,34 @@
+
+public class wk1VarsAndOps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// create var for availabe plane seats
+		int availablePlaneSeats = 6;
+		// var for cost of groceries
+		double costOfGroceries = 23.64;
+		// var to hold a single character
+		char middleInitial = 'M';
+		// var for true false boolean
+		boolean isHostOutside = false;
+		// var hold customers first name
+		String customersFirstName = "Billy";
+		// var hold street address
+		String streetAddress = "1313 MockingBird Lane";
+		
+		//print out all vars
+		System.out.println(availablePlaneSeats);
+		System.out.println("dCost of the groceris are:" + " " + costOfGroceries);
+		System.out.println("My Middle initial is:" + " " + middleInitial);
+		System.out.println("Is it hot outside?" + " " + isHostOutside);
+		System.out.println("Customers first name is:" + " " + customersFirstName);
+		System.out.println("My address is:" + " " + streetAddress);
+		
+		int num1 = 5;
+		int num2 = 2;
+		System.out.println(num1 / num2);
+		
+		
+	}
+
+}
